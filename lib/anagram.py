dadb9bac464866fc7b9d2550d30ea67ba3e49869
VenusBridget <venusbri27@gmail.com>
2024-06-02 19:57:15 +0300

    Done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted(word.lower())
+
+    def match(self, possible_anagrams):
+        matched_array = []
+        namearray = [letter for letter in self.word]
+
+        for elem in possible_anagrams:
+            elem_letters = [letters for letters in elem]
+            if sorted(elem_letters) == sorted (namearray):
+                matched_array.append(elem)
+            else:
+                return matched_array
+        return matched_array
